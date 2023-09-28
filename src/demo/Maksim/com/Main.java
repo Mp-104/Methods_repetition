77c472e49893a833a0dda750bc93566382a83204
Maksim <Mp-104@hotmail.com>
2023-09-28 13:27:20 +0200

    first commit

---
+++ src/demo/Maksim/com/Main.java
@@ -0,0 +1,21 @@
+package demo.Maksim.com;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Classes + object in main
+        //  (Create class,
+        //      to object,
+        //      to within main)
+
+
+        // Split up methods
+        // Argument/Params
+        // keywords ? methods (static, this)
+        // Methods that repeat until: "yes" / "no"
+        // Restart program if done () (question YES NO also)
+
+
+    }
+
+}
